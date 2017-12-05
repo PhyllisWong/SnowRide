ddfdd9335190c392e3efaaddf43e421ed66be5c8
Phyllis Wong <wong.phyllism@gmail.com>
2017-12-05 13:32:38 -0800

    update UI to include signup function

--- SnowRide/SnowRide/UserDataModel.swift
+++ SnowRide/SnowRide/UserDataModel.swift
@@ -10,24 +10,28 @@ import Foundation
 
 struct User {
     let id: String
+    let userName: String // "pwong"
+    let phone: Int //5555555555
+    
     let firstName: String // "firstName"
     let lastName: String // "lastName"
-    let userName: String // "pwong"
-    let password: String // "password"
     let email: String // "phyllis@gmail.com"
-    let phone: Int //5555555555
+    
+    let password: String // "password"
 }
 
 extension User: Decodable {
     
     enum UserKeys: String, CodingKey {
         case id = "_id"
+        case userName
+        case phone
+        
         case firstName
         case lastName
-        case userName
-        case password
         case email
-        case phone
+        
+        case password
     }
     
     init(from decoder: Decoder) throws {
@@ -35,25 +39,31 @@ extension User: Decodable {
         let container = try decoder.container(keyedBy: UserKeys.self)
         let userID: String =  try container.decode(String.self, forKey: .id)
         
-        let firstName: String = try container.decode(String.self, forKey: .firstName)
-        let lastName: String = try container.decode(String.self, forKey: .lastName)
         let userName: String = try container.decode(String.self, forKey: .userName)
-        let password: String = try container.decode(String.self, forKey: .password)
-        let email: String = try container.decode(String.self, forKey: .email)
         let phone: Int = try container.decode(Int.self, forKey: .phone)
         
-        self.init(id: userID, firstName: firstName, lastName: lastName, userName: userName, password: password, email: email, phone: phone)
+        let firstName: String = try container.decode(String.self, forKey: .firstName)
+        let lastName: String = try container.decode(String.self, forKey: .lastName)
+        let email: String = try container.decode(String.self, forKey: .email)
+        
+        let password: String = try container.decode(String.self, forKey: .password)
+        
+        self.init(id: userID, userName: userName, phone: phone, firstName: firstName, lastName: lastName, email: email, password: password)
     }
 }
+
+
 
 extension User: Encodable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: UserKeys.self)
+        try container.encode(userName, forKey: UserKeys.userName)
+        try container.encode(phone, forKey: UserKeys.phone)
+        
         try container.encode(firstName, forKey: UserKeys.firstName)
         try container.encode(lastName, forKey: UserKeys.lastName)
-        try container.encode(userName, forKey: UserKeys.userName)
-        try container.encode(password, forKey: UserKeys.password)
         try container.encode(email, forKey: UserKeys.email)
-        try container.encode(phone, forKey: UserKeys.phone)
+        
+        try container.encode(password, forKey: UserKeys.password)
     }
 }
--- SnowRide/toDo.md
+++ SnowRide/toDo.md
@@ -21,3 +21,40 @@ next mtg Monday during super Core
 1. Date picker updates the tableView
 1. Create model file for temp data
 1. Potential matches tableView VC
+
+
+### Pitch
+1. The problem
+    - Not all people in the Bay Area have cars, and certainly not all people who
+    like to go to Tahoe have cars.
+    - Some people who do drive to Tahoe want to have help off-setting the cost of
+    the drive to Tahoe, plus it is dangerous to make that drive alone.
+
+1. How it is solved today
+    - Snow Pals is a website that matches drivers and riders. The problem with this
+    solution, is that is not used by very many people. This makes finding a match
+    nearly impossible.
+
+1. How we are solving it
+    - We are developing a mobile app so that more people with be able to find matches
+    to share rides to Kirkwood.
+
+1. Benefits to our solution
+    - The app can give you alerts to your phone when a match has been made. Since it is
+    a mobile app, users are more encouraged to try the service.
+
+### Who our audience is
+1. Age: 18 - 35
+1. Outdoorsy types who ski or snowboard at an intermediate to advanced level
+1. Live in the Bay Area
+
+### Challenges to content based design
+1. Getting the content, working examples
+1. Design can take longer as you get working content
+1. Changes in content
+1. Scalability
+1. Misunderstandings in content
+
+### Things to avoid
+1. Navigation bar
+1. Shadows
